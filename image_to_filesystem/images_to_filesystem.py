9890fc18438ba686b51361a5ae543fcdb3b04d86
root <root@faceDetector.ucberkeley.cloud>
2019-09-17 17:03:46 +0000

    adjustments in python

--- image_to_filesystem/images_to_filesystem.py
+++ image_to_filesystem/images_to_filesystem.py
@@ -1,6 +1,13 @@
 import numpy as np
 import paho.mqtt.client as mqtt
 import time
+import pickle
+#import cv2
+
+
+
+from PIL import Image 
+import datetime
 
 #Create mqtt functions
 def on_connect(client, userdata, flags, rc):
@@ -17,7 +24,21 @@ def on_connect(client, userdata, flags, rc):
         print("Connection failed")
 
 def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
+#    frame = pickle.loads(message.payload, fix_imports=True, encoding="bytes")
+#    frame = cv2.imdecode(frame, cv2.COLOR_BGR2GRAY)
+#    filename = "faceDetectorBucket/"+datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")+'.jpg'
+#    cv2.imwrite(filename, frame)
+    print("message received " ,pickle.loads(message.payload.decode("utf-8")))
+    filename = "faceDetectorBucket/"+datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")+'.jpg'    
+#    cv2.imwrite(filename, pickle.loads(message.payload.decode("utf-8")))
+
+    pil_img = Image.fromarray(pickle.loads(message.payload.decode("utf-8")))
+    print(filename) 
+    
+
+
+    pil_img.save(filename)
+    print("message received")
     print("message topic=",message.topic)
     print("message qos=",message.qos)
     print("message retain flag=",message.retain)
@@ -29,18 +50,33 @@ Connected         = False
 broker            = "broker_cloud"
 
 port              = 1883
-user              = "face_detector"
-password          = "facew255"
+
+
+
+#user              = "face_detector"
+#password          = "facew255"
 client            = mqtt.Client("Python")
 client.on_connect = on_connect
-
+client.on_message = on_message
+#client.username_pw_set(user, password=password)
+client.connect(broker, port=port)
 client.loop_start()
 
 client.subscribe("w251/face_detector")
 
-time.sleep(4)
+#try:
+while True:
 
-client.loop_stop()
+    time.sleep(1)
+ 
+#except KeyboardInterrupt:
+   # print "exiting"
+   # client.disconnect()
+   # client.loop_stop()
+
+#time.sleep(4)
+
+#client.loop_stop()
 
 
 
